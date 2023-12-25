b208936e4cc469bdb71b3710060da68595960355
Dundo <107737568+ganud@users.noreply.github.com>
2023-12-25 12:05:04 -0500

    Finalize code

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ class Node {
 }
 
 function getMoves(coord) {
-  let moves = [];
-  let x = parseInt(coord[1]);
-  let y = parseInt(coord[3]);
+  const moves = [];
+  // Takes number from a string version of coords
+  const x = parseInt(coord[1]);
+  const y = parseInt(coord[3]);
+
+  // All possible knight moves
   moves.push([x + 1, y - 2]);
   moves.push([x + 1, y + 2]);
   moves.push([x + 2, y - 1]);
@@ -20,9 +23,11 @@ function getMoves(coord) {
   moves.push([x - 2, y + 1]);
   moves.push([x - 1, y - 2]);
   moves.push([x - 1, y + 2]);
-  let validmoves = []
+
+  // Remove out of bound moves
+  const validmoves = [];
   for (let i = 0; i < moves.length; i++) {
-    let move = moves[i]
+    const move = moves[i];
     if (!(move[0] < 0 || move[1] < 0) || (move[0] > 8 || move[1] > 8)) {
       validmoves.push(move);
     }
@@ -31,30 +36,43 @@ function getMoves(coord) {
 }
 
 function knightMoves(start, end) {
-  let visited = []
-  let queue = [start];
-
+  const visited = [];
+  const queue = [new Node(start)];
+  // Level order search using queue and unexplored moves
   while (queue.length > 0) {
     const current = queue[0];
     visited.push(current);
-    if (JSON.stringify(current) === JSON.stringify(end)) {
-      console.log('success')
+    if (JSON.stringify(current.coord) === JSON.stringify(end)) {
       break;
-    }
-    else {
-      let possibleMoves = getMoves(`[${current}]`);
-      possibleMoves.forEach(move => {
-        if (!visited.includes(move)) {
+    } else {
+      const possibleMoves = getMoves(`[${current.coord}]`);
+      possibleMoves.forEach((coord) => {
+        // Unvisited moves added to the queue
+        if (!visited.includes(new Node(coord))) {
+          const move = new Node(coord);
+          move.prev = current; // All moves link to the previous to reconstruct path later
           queue.push(move);
         }
       });
     }
     queue.shift();
   }
-  console.log(visited)
 
-
+  let temp = visited[visited.length - 1]; // Get the end node
+  let path = [];
+  let count = 0;
+  // Trail back to the start using the prev node links
+  while (temp !== null) {
+    path.push(temp.coord);
+    temp = temp.prev;
+    count++;
+  }
+  // Reconstruct path
+  path = path.reverse();
+  console.log(`You made it in ${count - 1} moves! Here is the path:`);
+  path.forEach((coord) => {
+    console.log(coord);
+  });
 }
 
-knightMoves([0,0], [1,4]) // Expected output ([1, 2], [3, 3], [1, 4])
-
+knightMoves([3, 0], [4, 3]); // Expected output ([3, 0], [5, 1], [4, 3])
